2b38403950064200fc229df7720d549284087e7a
Yekin Zulfikari <yekin_zulfikar@outlook.com>
2026-04-20 21:46:06 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/gps/gps_tracking_simulation_backend/GpsTrackingSimulationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gps.gps_tracking_simulation_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsTrackingSimulationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsTrackingSimulationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gps/gps_tracking_simulation_backend/GpsTrackingSimulationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gps.gps_tracking_simulation_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsTrackingSimulationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
